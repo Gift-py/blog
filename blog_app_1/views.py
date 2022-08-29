731c2a6204a84d28133389ea593e2387307b6405
Cultured <giftabumere247@gmail.com>
2022-08-29 09:31:43 +0100

    Day 3

--- blog_app_1/views.py
+++ blog_app_1/views.py
@@ -1,11 +1,27 @@
+from enum import _auto_null
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.models import User
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 3) #3 blogs per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer deliver the firt page (Almost like default page)
+        posts = paginator.page(1)
+    except EmptyPage:
+        #if page is out pf range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
-    
+
+
+
+
