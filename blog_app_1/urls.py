2345111bb08ec519b3ed3d76ad994dd00f5ccb98
Cultured <giftabumere247@gmail.com>
2022-08-29 10:12:29 +0100

    classes

--- blog_app_1/urls.py
+++ blog_app_1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'blog_app_1'
 
-urlpatterns = [path('', views.post_list, name='post_list'),
-                path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),]
+urlpatterns = [#path('', views.post_list, name='post_list'),
+               path('', views.PostListView.as_view(), name='post_list'),
+               path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),]
