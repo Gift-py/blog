6ad53093fd23852fb5db92a8f665965a9b8dd62d
Cultured <giftabumere247@gmail.com>
2022-08-30 01:03:21 +0100

    emails

--- blog_app_1/views.py
+++ blog_app_1/views.py
@@ -1,9 +1,12 @@
 from enum import _auto_null
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from django.views.generic import ListView
+
+from .models import Post
+from .forms import EmailPostForm
+
 
 # def post_list(request):
 #     object_list = Post.objects.all()
@@ -30,6 +33,14 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
 
-
-
-
+def post_share(request, post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        #form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post':post, 'form':form})
