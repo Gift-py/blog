c6d2954dfb81d8c129ebe8005425d092265e6382
Cultured <giftabumere247@gmail.com>
2022-08-31 18:04:07 +0100

    Tagging

--- blog_app_1/views.py
+++ blog_app_1/views.py
@@ -1,33 +1,40 @@
-from enum import _auto_null
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 from .models import Post, Comments
 from .forms import EmailPostForm, CommentForm
 
 
-# def post_list(request):
-#     object_list = Post.objects.all()
-#     paginator = Paginator(object_list, 3) #3 blogs per page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         #if page is not an integer deliver the firt page (Almost like default page)
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         #if page is out pf range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'posts':posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.objects.all()
+    tag = None
+    tags = Post.tags.all()
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in = [tag])
 
-class PostListView(ListView):
-    queryset =  Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+    paginator = Paginator(object_list, 3) #3 blogs per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer deliver the firt page (Almost like default page)
+        posts = paginator.page(1)
+    except EmptyPage:
+        #if page is out pf range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request, 'blog/post/list.html', {'posts':posts, 'page':page, 'tag':tag, 'tags':tags})
+
+# class PostListView(ListView):
+#     queryset =  Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -67,4 +74,5 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
+
 
